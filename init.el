32a1591e292bf0a02a3eace434127d095b15908f
nonissue <andy@nonissue.org>
2017-01-11 22:21:12 -0700

    added custom.el to remove gross custom set variables from init.el

--- init.el
+++ init.el
@@ -25,6 +25,9 @@
 
 (setq use-package-always-ensure t)
 
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 ;; load other settings
 (add-to-list 'load-path "~/.emacs.d/settings")
 
@@ -33,20 +36,3 @@
 (require 'key-bindings)
 (require 'defuns)
 (require 'skunkworks) ;; stuff in progress im playing with
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ivy-mode t)
- '(package-selected-packages
-   (quote
-    (counsel swiper spaceline spaceline-config ivy key-chord use-package heroku-theme heroku doom-themes))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
